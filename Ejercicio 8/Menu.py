31903309f68372c1779d63f20012548d9687d0aa
Alex-FOB <103607209+Alex-FOB@users.noreply.github.com>
2022-05-04 20:00:38 -0300

    Add files via upload

---
+++ Ejercicio 8/Menu.py
@@ -0,0 +1,27 @@
+from Manejador import manejador
+
+class interfaz:
+    __funcion = None
+    __op = None
+    __manejador = None
+    def __init__(self):
+        self.__op = {1: self.punto1, 2: self.punto2, 3: self.punto3, 4: self.salir}
+        self.__manejador = manejador()
+    def opcion(self, op):
+        self.__funcion = self.__op.get(op)
+        if self.__funcion:
+            self.__funcion()
+        else:
+            print('ERROR: opcion invalida')
+    def punto1(self):
+        print('A + B = {}'.format(self.__manejador.punto1()))
+        input()
+    def punto2(self):
+        print('A - B = {}'.format(self.__manejador.punto2()))
+        input()
+    def punto3(self):
+        print('A == B = {}\nA == C = {}'.format(self.__manejador.punto3(True), self.__manejador.punto3(False)))
+        input()
+    def salir(self):
+        print('FINALIZANDO...')
+        input()
